f8415a3958a82d64ff2fc3899f872223f06c11aa
Raushankumar2004 <161424227+Raushankumar2004@users.noreply.github.com>
2024-03-12 17:40:33 +0530

    Update Mergesort.c

--- Mergesort.c
+++ Mergesort.c
@@ -1,54 +1,58 @@
-#include<iostream>
-using namespace std;
-void merge(int arr[],int l,int mid,int r){ 
-    int n1 = mid-l+1;
-    int n2 = r-mid;
+#include <stdio.h>
+
+void merge(int arr[], int l, int mid, int r) {
+    int n1 = mid - l + 1;
+    int n2 = r - mid;
     int a[n1];
-    int b[n2]; // temp array 
-    for(int i=0;i<n1;i++){
-        a[i] = arr[l+i];
-    }  
-    for(int i=0;i<n2;i++){
-        b[i]=arr[mid+1+i];
-    }   
-    int i = 0;
-    int j = 0;
-    int k = l;
-    while(i<n1 && j<n2){
-        if(a[i]<b[j]){
+    int b[n2];
+    int i, j, k;
+
+    for (i = 0; i < n1; i++)
+        a[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        b[j] = arr[mid + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (a[i] < b[j]) {
             arr[k] = a[i];
-            k++; i++;
-        } 
-        else{
+ k++;
+            i++;
+        } else {
             arr[k] = b[j];
-            k++; j++;
+            k++;
+            j++;
         }
-    }  
-    while(i<n1){
+    }
+    while (i < n1) {
         arr[k] = a[i];
-        k++; i++;
-    }  
-    while(j<n2){
+        k++;
+        i++;
+    }
+    while (j < n2) {
         arr[k] = b[j];
-        k++; j++;
+        k++; 
+j++;
     }
+}
 
-} 
-void mergesort(int arr[],int l,int r){
-    if(l<r){
-        int mid = (l+r)/2;
-        mergesort(arr,l,mid);
-        mergesort(arr,mid+1,r);
-
-        merge(arr,l,mid,r);
+void mergesort(int arr[], int l, int r) {
+    if (l < r) {
+        int mid = (l + r) / 2;
+        mergesort(arr, l, mid);
+        mergesort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
     }
-}  
-int main(){
-    int arr[] = {5,4,3,2,1};
-    mergesort(arr,0,4);
-    for(int i=0;i<5;i++){
-        cout<<arr[i]<<" ";
-    }  
-    cout<<endl;
+}
+
+int main() {
+    int arr[] = {5, 4, 3, 2, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    mergesort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
     return 0;
 }
